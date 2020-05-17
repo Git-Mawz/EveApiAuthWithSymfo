841a3b9f05c9097133cc70dd118a076e6b120fd6
Git-Mawz <py.oclock@gmail.com>
2020-05-17 14:29:02 +0200

    fetch image on character/details.html.twig

--- src/Controller/CharacterController.php
+++ src/Controller/CharacterController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Services\EsiEndpoints;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class CharacterController extends AbstractController
 {
     /**
      * @Route("/character/details", name="character_details")
      */
-    public function details()
+    public function details(SessionInterface $session)
     {   
-    
+        $user = $session->get('user');
+        $characterID = $user->getCharacterID();
+
+        $esiBaseUrl = 'https://esi.evetech.net/latest/';
+        $characterPortrait = $esiBaseUrl . 'characters/' .$characterID. '/portrait/';
+
+        dump($characterID);
+        $characterPortraits = json_decode(file_get_contents($characterPortrait));
 
         return $this->render('character/details.html.twig', [
-            
+            'portraits' => $characterPortraits
         ]);
     }
 }
--- src/Services/EsiEndpoints.php
+++ src/Services/EsiEndpoints.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace App\Services;
+
 use Evelabs\OAuth2\Client\Provider\EveOnline;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class EsiEndpoints {
 
 
-    
-    
+    // Eve ESI base URL
+    public $baseEsiUrl = 'https://esi.evetech.net/latest/';
 
 }
