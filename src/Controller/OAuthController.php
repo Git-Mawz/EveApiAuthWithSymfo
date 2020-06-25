d6ded6dea3a3fc0e61b01d7f70bd8aae3484ba84
Git-Mawz <py.oclock@gmail.com>
2020-06-25 12:00:20 +0200

    going an other way to manage capsuler portraits

--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Capsuler;
 use App\Repository\CapsulerRepository;
+use App\Services\PortraitUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\Routing\Annotation\Route;
