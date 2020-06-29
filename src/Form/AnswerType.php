e63140c953daf2d2958a3719bff63040af8cf445
Git-Mawz <py.oclock@gmail.com>
2020-06-29 15:14:16 +0200

    working on WSL

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -7,6 +7,7 @@ use App\Entity\Question;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,7 @@ class AnswerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content', TextType::class, [
+            ->add('content', TextareaType::class, [
                 'label' => 'Votre réponse'
             ])
             ->add('submit', SubmitType::class, [
