66b533a0d311d4ec698424c4240e38ced8203e42
Git-Mawz <py.oclock@gmail.com>
2020-07-09 11:46:35 +0200

    added Capsuler as the app_user_provider

--- src/Entity/Capsuler.php
+++ src/Entity/Capsuler.php
@@ -6,11 +6,12 @@ use App\Repository\CapsulerRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=CapsulerRepository::class)
  */
-class Capsuler
+class Capsuler implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -53,6 +54,11 @@ class Capsuler
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $portrait;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
 
 
     public function __construct()
@@ -110,6 +116,25 @@ class Capsuler
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
@@ -186,5 +211,35 @@ class Capsuler
         $this->portrait = $portrait;
 
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword()
+    {
+        // not needed for apps that do not check user passwords
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed for apps that do not check user passwords
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getUsername(): string
+    {
+        return (string) $this->username;
     }
 }
